36d7e54ca71bdc914637b709756a26e9c6118301
nataliestam <natalielynnstam@gmail.com>
2018-05-26 15:50:00 -0700

    initialize App component

---
+++ client/src/components/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <h1>hellohello</h1>
+      </div>
+    )
+  }
+}
+
+export default App;
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './components/App.jsx';
 
-ReactDOM.render(<h1>hellooooo!</h1>, document.getElementById('app'));
-
+ReactDOM.render(<App />, document.getElementById('app'));
