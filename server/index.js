3ba737f6d86536d420a85a2563159754917cfaf9
nataliestam <natalielynnstam@gmail.com>
2018-05-26 16:08:58 -0700

    add test data

--- server/index.js
+++ server/index.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const app = express();
 
+let movies = [
+  {title: 'Mean Girls'},
+  {title: 'Hackers'},
+  {title: 'The Grey'},
+  {title: 'Sunshine'},
+  {title: 'Ex Machina'},
+];
+
 app.use(express.static('./client/dist'));
 
 app.listen(3000, () => console.log('listening on port 3000!'));
