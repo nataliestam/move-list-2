eb8f7127129771759f28334c6a01444c74314de6
nataliestam <natalielynnstam@gmail.com>
2018-05-26 17:23:35 -0700

    implement MovieAdd componenet

---
+++ client/src/components/AddMovie.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AddMovie = (props) => (
+  <div>
+    <form>
+      <input placeholder="Add movie title here"></input>
+      <button>Add</button>
+    </form>
+  </div>
+)
+
+export default AddMovie;
--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -3,6 +3,7 @@ import 'bootstrap';
 import $ from 'jquery';
 import MovieList from './MovieList.jsx'
 import Search from './Search.jsx'
+import AddMovie from './AddMovie.jsx'
 
 class App extends React.Component {
   constructor(props) {
@@ -20,6 +21,16 @@ class App extends React.Component {
     this.getMovies();
   }
 
+  getMovies() {
+    $.ajax({
+      url: '/movies',
+      type: 'GET'
+    })
+    .done((data) => {
+      this.setState({movies: data, searchedMovies: data});
+    });
+  }
+
   searchMovies(searchMovie) {
     let search = searchMovie.toLowerCase();
     this.setState({
@@ -35,19 +46,10 @@ class App extends React.Component {
     });
   }
 
-  getMovies() {
-    $.ajax({
-      url: '/movies',
-      type: 'GET'
-    })
-    .done((data) => {
-      this.setState({movies: data, searchedMovies: data});
-    });
-  }
-
   render() {
     return (
       <div className="container">
+        <AddMovie />
         <Search onSubmit={this.searchMovies}/>
         {this.state.searchMessage}
         <MovieList movies={this.state.searchedMovies} />
